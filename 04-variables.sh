0aebb1127c5bac71de9a2687f7a744019b0471e6
narendra <narendrag0824@gmail.com>
2025-10-17 10:29:36 +0530

    variables started

---
+++ 04-variables.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+person1=$1
+person2=$2
+
+echo "$person1::hey hi hoe r u?"
+echo "$person2::im fine how r u?"
+echo "$person1::im good, ok what are you doing now"
+echo "$person2::present im doing job in cedge tech as assistent system analyst now i want to switch the company present im working on it"
+echo "$person1::ok thats nice what r u doing for switch the company"
+echo "$person2::now i, learning devsecops with aws"
+echo "$person1::oh ok thats good its nice to meet u, best of luck for ur future hope u get job very soon"
+echo "$person2::thanks $person1, hope u doing well,bye bye takecare"
+echo "$person1::bye $person2."
