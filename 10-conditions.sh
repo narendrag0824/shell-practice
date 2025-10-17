4231656e1f22362255e1bb153431c83be71ef4c2
narendra <narendrag0824@gmail.com>
2025-10-17 18:43:39 +0530

    mesg

--- 10-conditions.sh
+++ 10-conditions.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 echo " please enter the number"
-read number=$1
-if [$(($number % 2)) eq 0]; then
+read number
+if [$(($number % 2)) -eq 0]; then
     echo " the given $number is even"
 else
     echo " then given $number is odd"
