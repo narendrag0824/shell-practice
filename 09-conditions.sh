885c54a2179f5b0067f639c3474382913a02b61e
narendra <narendrag0824@gmail.com>
2025-10-17 18:25:44 +0530

    mesg

--- 09-conditions.sh
+++ 09-conditions.sh
@@ -2,7 +2,9 @@
 
 number=$1
  if [ $number -lt 10 ]; then 
-     echo" the given number $number is less than 10"
+     echo "the given number $number is less than 10"
+ elif[ $number -eq 10]; then
+     echo "the given number $number is equal to 10"    
  else
-     echo" given number $number is greater than or eqal to 10"
+     echo "given number $number is greater than 10"
  fi       
