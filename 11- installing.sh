b2d960757068ffa0d6090da467750e1da823be51
narendra <narendrag0824@gmail.com>
2025-10-17 20:26:50 +0530

    mesg

---
+++ 11- installing.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#if id is zero then proceed otherwise stop
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]; then
+   echo "error::pls run as a sudo user"
+   exit 1
+fi
+
+dnf install mysql -y
+
+if [$? -ne 0 ];then
+  echo "error::installing is failed"
+  exit 1
+  
+ else
+   echo "installing my sql server is success"
+   exit 0
+ fi   
