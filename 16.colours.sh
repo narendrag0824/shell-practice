5bd225d275c26bbb54194477f4477f00ca43752e
narendra <narendrag0824@gmail.com>
2025-10-22 09:17:37 +0530

    shell

---
+++ 16.colours.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+
+r="\e[31m"
+g="\e[32m"
+y="\e[33m"
+n="\e[0m"
+
+userid=$(id -u)
+
+if [ $userid -ne 0 ]; then
+    echo -e "error::please run as sudo user"
+    exit 1
+fi
+
+validate(){
+  if [ $1 -ne 0 ]; then
+      echo -e "installing $2..$r is failed $n"
+      exit 1
+  else
+      echo -e "installing $2..$g is sucess $n"
+  fi      
+}
+
+dnf list installed mysql
+if [ $? -ne 0]; then
+dnf install mysql -y
+validate $? "mysql"
+else  
+   echo-e "mysql is already exist..$y skipping $n"
+fi   
+
+dnf list installed nginx
+if [ $? -ne 0]; then
+dnf install nginx -y
+validate $? "nginx"
+else  
+    echo-e "nginx is already exist..$y skipping $n"
+fi   
+
+dnf list installed python3
+if [ $? -ne 0]; then
+dnf install python3 -y
+validate $? "python3"
+else  
+    echo-e "python3 is already exist..$y skipping $n"
+fi   
