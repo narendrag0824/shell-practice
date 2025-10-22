7107ba15dad5630ddc8f5e113d0496b885ba5fad
narendra <narendrag0824@gmail.com>
2025-10-22 08:22:02 +0530

    shell

---
+++ 15.functions.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+userid=$(id -u)
+
+if [ $userid _-ne 0 ]; then
+    echo "error::please run as sudo user"
+    exit 1
+fi
+
+validate(){
+  if [ $1 -ne 0 ]; then
+      echo "error::installing $2 is failed"
+      exit 1
+  else
+      echo "installing $2 is sucess"
+  fi      
+}
+
+dnf install mysql -y
+validate $? "mysql"
+
+dnf install nginx -y
+validate $? "nginx"
+
+dnf install python3 -y
+validate $? "python3"
